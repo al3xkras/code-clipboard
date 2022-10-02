fde3b0a785a4ec80f27712e082011fa8f6b17c6f
al3xkras <alexandr.krasovskiy@gmail.com>
2022-10-02 17:03:50 +0200

    Move javascript code to self-contained files

---
+++ src/main/resources/static/js/search.js
@@ -0,0 +1,71 @@
+const langToggle=document.getElementById("language-toggle");
+const substringToggle=document.getElementById("substring-toggle");
+const tagsToggle=document.getElementById("tags-toggle");
+
+const langSelect=document.getElementById("language")
+const substringInput=document.getElementById("substring")
+const tagDelimiterInput=document.getElementById("tag-delim")
+const tagsInput=document.getElementById("tags")
+
+const toggleSwitch = function (inputCheckbox,...elements) {
+    inputCheckbox.addEventListener("click",e=>{
+        if (inputCheckbox.checked){
+            elements.forEach(e=>{
+                e.disabled=null;
+            });
+        } else {
+            elements.forEach(e=>{
+                e.disabled=1;
+            });
+        }
+    })
+}
+toggleSwitch(langToggle,langSelect);
+toggleSwitch(substringToggle,substringInput);
+toggleSwitch(tagsToggle,tagDelimiterInput,tagsInput);
+
+document.getElementById("search-button").addEventListener("click", e=>{
+    const l = langToggle.checked;
+    const s = substringToggle.checked;
+    const t = tagsToggle.checked;
+    if (l && !s && !t){
+        alert("Unable to search code by programming language only");
+        return;
+    } else  if (!l && !s && !t){
+        alert("Please choose at least one search option");
+        return;
+    }
+    const data = new FormData();
+    if (l){
+        const lang = langSelect.options[langSelect.selectedIndex].text;
+        data.append("language",lang);
+    }
+    if (s){
+        const substr = substringInput.value;
+        data.append("substring",substr);
+    }
+    if (t){
+        let delim = " ";//tagDelimiterInput.value
+        if (delim.length===0) {
+            delim=" ";
+        }
+        console.log(delim)
+        let tags = ""+tagsInput.value;
+        tags=tags.replaceAll(new RegExp(delim,'g')," ");
+        data.append("tags",tags);
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('POST', '/search', true);
+    xhr.onload = function () {
+        console.log(xhr.status);
+        if (xhr.status===200){
+            let jsonResponse = JSON.parse(xhr.responseText);
+            console.log(xhr.responseText);
+        } else {
+            alert(xhr.status)
+        }
+    };
+    xhr.send(data);
+});
