77d2a5a4933d4f8480b33bc3ff1e1a95c68e544d
al3xkras <alexandr.krasovskiy@gmail.com>
2022-10-02 13:57:33 +0200

    Create new pages, edit PagesController

--- src/main/java/com/al3xkras/code_clipboard/controller/PagesController.java
+++ src/main/java/com/al3xkras/code_clipboard/controller/PagesController.java
@@ -2,8 +2,24 @@ package com.al3xkras.code_clipboard.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class PagesController {
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String codeSearchPage(){
+        return "search";
+    }
+
+    @GetMapping("/submit")
+    public String codeSubmissionPage(){
+        return "submit";
+    }
 }
