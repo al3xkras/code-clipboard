8488fb866340c397a2a9c3d03ca9db81e95e1de9
al3xkras <alexandr.krasovskiy@gmail.com>
2022-10-04 21:40:49 +0200

    (windows): open application URL on launch

--- src/main/java/com/al3xkras/code_clipboard/CodeClipboardApplication.java
+++ src/main/java/com/al3xkras/code_clipboard/CodeClipboardApplication.java
@@ -1,14 +1,29 @@
 package com.al3xkras.code_clipboard;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Slf4j
 @SpringBootApplication
 public class CodeClipboardApplication {
 	public static final String delimiter="\u0001";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException, IOException {
 		SpringApplication.run(CodeClipboardApplication.class, args);
+
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.contains("win")){
+			Runtime rt = Runtime.getRuntime();
+			String url = "http://localhost:10000";
+			rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+		}
+
 	}
 
 }
